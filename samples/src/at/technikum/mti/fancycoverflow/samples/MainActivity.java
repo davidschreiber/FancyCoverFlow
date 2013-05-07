c649b6e5dd2625a341c868b0e78767fca645d939
David Schreiber <david.schreiber@blockhaus-medien.at>
2013-05-07 06:03:17 +0200

    Update: Added basic structure with library and sample projects.

---
+++ samples/src/at/technikum/mti/fancycoverflow/samples/MainActivity.java
@@ -0,0 +1,15 @@
+package at.technikum.mti.fancycoverflow.samples;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
